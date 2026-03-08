bd9c018513161ac4ef7a10b35f9858407e5b603b
rklfc07 <rakesh.lfc@gmail.com>
2026-03-08 17:14:40 +0530

    logo and portfolio updated

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -71,7 +71,7 @@ const Layout = () => {
             <img
               src="/images/logo.png"
               alt="Results Over Noise"
-              className="h-18 lg:h-20 w-auto object-contain"
+              className="h-8 sm:h-10 lg:h-16 w-auto object-contain"
             />
           </Link>
 
